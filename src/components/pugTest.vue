75797ae43c0b241b2ecf12a2678d89e8694bccde
OZida <nakuzida@gmail.com>
2018-02-27 23:43:11 +0200

    seventh homework

--- src/components/pugTest.vue
+++ src/components/pugTest.vue
@@ -1,8 +1,12 @@
 <template lang='pug'>
-	div
-		div {{msg}}
+	div#pugTest
 		input#file(type='file')
 		p {{file.message}}
+		input#image(type='file')
+		br
+		img#picture
+		br
+		canvas#container_for_img
 </template>	
 
 
@@ -10,17 +14,23 @@
 	export default {
 		data(){
 			return {
-			msg: "hallo!",
-			file: {message: ''}
+			file: {message: ''},
+			image: ''
 			}
 		},
 		mounted() {
-				let input = document.getElementById('file');
-				let output = this.file;
-				// console.log(input);
+				let input = document.getElementById('file'),
+					output_first = this.file,
+					image = document.getElementById('image'),
+					picture = document.getElementById('picture'),
+					output_second = this.file;
+
+				let container_for_img = document.getElementById('container_for_img'),
+					cont = container_for_img.getContext('2d'),
+					pic = new Image();
+
 
 				input.onchange = function(){
-
 					// console.log(this);
 					let list = this.files;
 					let file = list[0];
@@ -29,13 +39,43 @@
 
 					reader.onloadend = function(event){
 						let text = event.target.result;
-						console.log(text);
-						output.message = text;
+						// console.log(text);
+						output_first.message = text;
 					}
 					reader.readAsText(file);
+				}
+
+				image.onchange = function(){
+					let list = this.files;
+					let file = list[0];
+					let reader = new FileReader();
+
+					reader.onloadend = function(event){
+						let img = event.target.result;
+						// console.log(img);
+						output_second = picture.setAttribute("src", img);
+					}
+					reader.readAsDataURL(file);
+
+					picture.onload = function () {
+						cont.clearRect(0, 0, container_for_img.width, container_for_img.height);
+						pic.src = picture.getAttribute('src');
+						cont.drawImage(pic, 0, 0);
+					}
 				}
 		
 		}
 	}
 
 </script>
+
+<style lang='scss' scoped> 
+
+@import '../styles/_reset.scss';
+@import '../styles/_var.scss';
+
+	#pugTest {
+		@extend .interface_by_Vue;
+	}
+
+</style>
